427ebf39226a4bf07a95157e4b894508d8a33062
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-03 23:45:15 +0300

    Remade tailwind packages with configs

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,6 @@ export default defineConfig({
       '@pages': resolve(__dirname, 'src/pages'),
     },
   },
-  css: {
-    postcss: './postcss.config.js',
-  },
   server: {
     port: 3000,
     open: true,
