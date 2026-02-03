7fbade2bf04ee8896da3121c7f66072c0f67fe6e
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-03 20:39:51 +0300

    Add aliases configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@hooks': resolve(__dirname, 'src/hooks'),
+      '@lib': resolve(__dirname, 'src/lib'),
+      '@utils': resolve(__dirname, 'src/utils'),
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@pages': resolve(__dirname, 'src/pages'),
+    },
+  },
   css: {
     postcss: './postcss.config.js',
   },
