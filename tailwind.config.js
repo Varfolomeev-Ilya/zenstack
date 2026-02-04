4aff02059b940f8e2092edf12ddf5391a9c1d0ab
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-04 12:18:25 +0300

    Setup theme color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // ← ТОЛЬКО ЭТО!
-  ],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          primary: 'var(--color-background-primary)',
+          secondary: 'var(--color-background-secondary)',
+        },
+        active: 'var(--color-active)',
+        error: {
+          primary: 'var(--color-error-primary)',
+          secondary: 'var(--color-error-secondary)',
+        },
+        success: {
+          primary: 'var(--color-success-primary)',
+          secondary: 'var(--color-success-secondary)',
+        },
+        text: {
+          primary: 'var(--color-text-primary)',
+          secondary: 'var(--color-text-secondary)',
+        },
+        border: {
+          primary: 'var(--color-border-primary)',
+        },
+      },
+
+      borderRadius: {
+        lg: '0.5rem',
+        md: 'calc(0.5rem - 2px)',
+        sm: 'calc(0.5rem - 4px)',
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
   },
   plugins: [],
 };
