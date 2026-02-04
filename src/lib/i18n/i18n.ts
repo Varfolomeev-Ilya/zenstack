46afeaf9d6022a9d4df58fdfb10a91050e507acc
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-04 23:16:20 +0300

    Fix directive typing

--- src/lib/i18n/i18n.ts
+++ src/lib/i18n/i18n.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-'use-client';
+'use client';
 import { FC, useEffect, useState } from 'react';
 
 import { Navigate } from 'react-router-dom';
