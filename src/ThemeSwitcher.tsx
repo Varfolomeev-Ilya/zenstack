a0ead3cafd468e509b1a0ef79c3f6647eb30d8c2
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-04 12:55:36 +0300

    Implement basic theme switcher

---
+++ src/ThemeSwitcher.tsx
@@ -0,0 +1,17 @@
+import { Moon, Sun } from 'lucide-react';
+
+import { useTheme } from './hooks/useTheme';
+
+export function ThemeSwitcher() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      className="w-9 h-9 rounded-lg flex items-center justify-center"
+      aria-label={`Switch to ${theme === 'dark' ? 'light' : 'dark'} theme`}
+      onClick={toggleTheme}
+    >
+      {theme === 'dark' ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5 " />}
+    </button>
+  );
+}
