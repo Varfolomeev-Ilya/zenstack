c37b5dfadda538cd9003e19b74c3bf2884a772c9
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-13 14:58:32 +0300

    Add basic login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,68 @@
 'use client';
+import { Mail, KeySquare } from 'lucide-react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { signInWithGoogle } from '@/api/auth';
+import { signInWithEmail, signInWithGoogle } from '@/api/auth';
+import { Button } from '@/components/ui/button';
+import AuthLayoutWrapper from '@/shared/AuthLayoutWrapper';
+import FormInput from '@/shared/FormInput';
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
   const { t } = useTranslation('auth');
+  const { control, handleSubmit } = useForm<ILoginForm>();
+
+  const onSubmit: SubmitHandler<ILoginForm> = data => {
+    const { email, password } = data;
+    signInWithEmail(email, password);
+  };
+
   return (
-    <div>
-      <button onClick={signInWithGoogle}>Login with Google</button>
-      <h1>Login</h1>
-      <p>{t('greet')}</p>
-    </div>
+    <AuthLayoutWrapper>
+      <div className="flex flex-col w-full max-w-[540px] p-5 border border-border rounded-[14px]">
+        <h1 className="mb-5 text-h3">{t('greet')}</h1>
+
+        <div className="flex flex-col w-full ">
+          <Button variant="outline" onClick={signInWithGoogle}>
+            Login with Google
+          </Button>
+        </div>
+
+        <div className="flex w-full items-center">
+          <hr className="color-gray w-full"></hr>
+          <span className="p-4">OR</span>
+          <hr className="color-gray w-full"></hr>
+        </div>
+
+        <form className="flex flex-col w-full gap-4" onSubmit={handleSubmit(onSubmit)}>
+          <FormInput
+            name="email"
+            control={control}
+            label="Email"
+            placeholder="email"
+            LeftIcon={Mail}
+            type="email"
+          />
+          <FormInput
+            name="password"
+            control={control}
+            label="Password"
+            placeholder="password"
+            LeftIcon={KeySquare}
+            type="password"
+          />
+
+          <Button variant="secondary" type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
+    </AuthLayoutWrapper>
   );
 };
 
