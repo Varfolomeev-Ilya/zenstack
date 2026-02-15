a68abfa356df0b093359b149e9c984e81eb7d848
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-15 13:58:08 +0300

    Add childern wrapper for AuthLayoutWraper

--- src/shared/AuthLayoutWrapper.tsx
+++ src/shared/AuthLayoutWrapper.tsx
@@ -13,7 +13,9 @@ const AuthLayoutWrapper: FC<IAuthLayoutWrapperProps> = ({ children }) => {
         <img src={AuthIllustrationSrc} alt="Auth illustration" />
       </div>
       <div className="flex flex-col items-center justify-center py-6 px-7 w-full text-foreground">
-        {children}
+        <div className="flex flex-col w-full max-w-[540px] p-5 border border-border rounded-[14px] bg-card text-card-foreground shadow-sm">
+          {children}
+        </div>
       </div>
     </div>
   );
