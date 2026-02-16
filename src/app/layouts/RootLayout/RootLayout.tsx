c8a9ca44462fdf669b1fec461c48dcc40a225862
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-16 11:21:04 +0300

    Setup error toast

--- src/app/layouts/RootLayout/RootLayout.tsx
+++ src/app/layouts/RootLayout/RootLayout.tsx
@@ -1,7 +1,11 @@
 'use client';
+
+import Toast from '@/features/auth/ui/Toast/Toast';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toast />
       <main className="w-full min-h-full px-4 py-8 bg-background text-foreground">{children}</main>
     </>
   );
---
+++ src/shared/hooks/useErrToast.ts
@@ -0,0 +1,93 @@
+import { useCallback } from 'react';
+
+import { AxiosError, CanceledError } from 'axios';
+import { toast } from 'react-toastify';
+
+import { getAxiosErrObj } from '@/shared/lib/api/getAxiosErrObj';
+
+const UNKNOWN_ERROR = 'Unknown Error';
+
+interface IApiErrorResponse {
+  error: {
+    message?: string;
+    code: number;
+    detail?: string;
+  };
+}
+
+const isApiErrorResponse = (err: unknown): err is IApiErrorResponse => {
+  return (
+    typeof err === 'object' &&
+    err !== null &&
+    'error' in err &&
+    typeof (err as IApiErrorResponse).error === 'object'
+  );
+};
+
+export const useErrToast = () => {
+  const showDefaultErrToast = useCallback((errText: string) => {
+    toast.error(`Error: ${errText}`);
+  }, []);
+
+  const checkApiErr = useCallback(
+    (err: IApiErrorResponse | undefined) => {
+      if (!err?.error) {
+        showDefaultErrToast(JSON.stringify(err));
+        return;
+      }
+
+      const { code, message, detail } = err.error;
+      const errorParts = [message, detail].filter(Boolean);
+      const errorText = `${code}: ${errorParts.join(' - ') || UNKNOWN_ERROR}`;
+
+      showDefaultErrToast(errorText);
+    },
+    [showDefaultErrToast],
+  );
+
+  const showErrToast = useCallback(
+    (err: unknown) => {
+      if (err instanceof CanceledError) {
+        return;
+      }
+
+      if (err instanceof AxiosError) {
+        const errData = getAxiosErrObj(err);
+        const data: IApiErrorResponse = {
+          error: {
+            code: errData?.code ?? errData?.status ?? 500,
+            message: errData?.msg ?? errData?.title,
+            detail: errData?.detail,
+          },
+        };
+        checkApiErr(data);
+        return;
+      }
+
+      if (err instanceof Error) {
+        showDefaultErrToast(err.message);
+        return;
+      }
+
+      if (isApiErrorResponse(err)) {
+        checkApiErr(err);
+        return;
+      }
+
+      if (typeof err === 'string') {
+        showDefaultErrToast(err);
+        return;
+      }
+
+      if (err && typeof err === 'object' && 'message' in err) {
+        showDefaultErrToast(String(err.message));
+        return;
+      }
+
+      showDefaultErrToast(UNKNOWN_ERROR);
+    },
+    [checkApiErr, showDefaultErrToast],
+  );
+
+  return { showErrToast };
+};
