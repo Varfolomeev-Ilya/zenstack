66b244efa63535fbc910b98708c5105a514b8cb3
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-16 08:49:04 +0300

    Add reusable email validation schema

--- src/pages/Login/Login.constants.ts
+++ src/pages/Login/Login.constants.ts
@@ -1,15 +1,10 @@
-import i18n from 'i18next';
 import * as z from 'zod';
 
+import { emailValidation } from '@/utils/checkEmailValidity';
 import { passwordValidation } from '@/utils/checkPasswordValidity';
 
 export const loginFormSchema = z.object({
-  email: z
-    .email({
-      message: i18n.t('errors:INCORRECT_EMAIL'),
-    })
-    .min(5, i18n.t('errors:EMAIL_MIN_LENGTH'))
-    .max(50, i18n.t('errors:EMAIL_MAX_LENGTH')),
+  email: emailValidation,
   password: passwordValidation,
 });
 
