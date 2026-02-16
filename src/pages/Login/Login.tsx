9b91cbd14ff779b3af87477494edd6a74fb94bcb
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-16 09:29:41 +0300

    Made reusable auth form for login and signup

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,97 +1,34 @@
 'use client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Mail, KeySquare } from 'lucide-react';
-import { SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 
-import { loginFormSchema, TLoginFormValues } from './Login.constants';
-
-import { signInWithEmail, signInWithGoogle } from '@/api/auth';
-import { Button } from '@/components/ui/button';
 import { ROUTES } from '@/constants';
-import AppLink from '@/shared/AppLink';
+import AuthForm from '@/shared/AuthForm/AuthForm';
 import AuthLayoutWrapper from '@/shared/AuthLayoutWrapper';
-import FormInput from '@/shared/FormInput';
 
 const Login = () => {
   const { t } = useTranslation('auth');
 
-  const { control, handleSubmit } = useForm<TLoginFormValues>({
-    resolver: zodResolver(loginFormSchema),
-    defaultValues: {
-      email: '',
-      password: '',
+  const links = [
+    {
+      helperTxt: t('login.noAccountYet'),
+      title: t('login.registration'),
+      path: ROUTES.SIGN_UP,
     },
-    mode: 'onChange',
-  });
-
-  const navigate = useNavigate();
-
-  const onSubmit: SubmitHandler<TLoginFormValues> = async data => {
-    try {
-      const { email, password } = data;
-      const response = await signInWithEmail(email, password);
-
-      if (response.data.session) {
-        navigate(ROUTES.HOME);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    {
+      helperTxt: t('login.forgotPassword'),
+      title: t('login.forgotPassword'),
+      path: ROUTES.FORGOT_PASSWORD,
+    },
+  ];
 
   return (
     <AuthLayoutWrapper>
-      <>
-        <h1 className="mb-5 text-h3 text-foreground">{t('login.title')}</h1>
-
-        <div className="flex flex-col w-full ">
-          <Button variant="outline" onClick={signInWithGoogle}>
-            {t('login.loginWithGoogle')}
-          </Button>
-        </div>
-
-        <div className="flex w-full items-center">
-          <hr className="flex-1 border-border" />
-          <span className="px-4 text-muted-foreground text-sm">{t('common:span')}</span>
-          <hr className="flex-1 border-border" />
-        </div>
-
-        <form className="flex flex-col w-full gap-3" onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col gap-2">
-            <FormInput
-              name="email"
-              control={control}
-              label={t('common:inputs.emailInput.label')}
-              placeholder={t('common:inputs.emailInput.placeholder')}
-              LeftIcon={Mail}
-              type="email"
-            />
-            <FormInput
-              name="password"
-              control={control}
-              label={t('common:inputs.passwordInput.label')}
-              placeholder={t('common:inputs.passwordInput.placeholder')}
-              LeftIcon={KeySquare}
-              type="password"
-            />
-          </div>
-
-          <div className="flex items-center justify-between text-muted-foreground text-sm">
-            <p>{t('login.noAccountYet')}</p>
-            <AppLink title={t('login.registration')} path={ROUTES.SIGN_UP} />
-          </div>
-          <div className="flex items-center justify-between text-muted-foreground text-sm">
-            <p>{t('login.forgotPassword')}</p>
-            <AppLink title={t('login.recoverPassword')} path={ROUTES.FORGOT_PASSWORD} />
-          </div>
-
-          <Button variant="secondary" type="submit">
-            {t('login.loginBtn')}
-          </Button>
-        </form>
-      </>
+      <AuthForm
+        title={t('login.title')}
+        googleBtnTxt={t('login.loginWithGoogle')}
+        submitBtnTxt={t('login.loginBtn')}
+        linksArr={links}
+      />
     </AuthLayoutWrapper>
   );
 };
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,84 +1,28 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Mail, KeySquare } from 'lucide-react';
-import { SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { loginFormSchema, TLoginFormValues } from '../Login/Login.constants';
-
-import { signUpWithEmail } from '@/api/auth';
-import { Button } from '@/components/ui/button';
-import { CONFIRM_EMAIL_LINK, ROUTES } from '@/constants';
-import AppLink from '@/shared/AppLink';
+import { ROUTES } from '@/constants';
+import AuthForm from '@/shared/AuthForm/AuthForm';
 import AuthLayoutWrapper from '@/shared/AuthLayoutWrapper';
-import FormInput from '@/shared/FormInput';
 
 const SignUp = () => {
   const { t } = useTranslation('auth');
-  const { control, handleSubmit } = useForm<TLoginFormValues>({
-    resolver: zodResolver(loginFormSchema),
-    defaultValues: {
-      email: '',
-      password: '',
+
+  const links = [
+    {
+      helperTxt: t('signUp.hasAccount'),
+      title: t('signUp.login'),
+      path: ROUTES.LOGIN,
     },
-    mode: 'onChange',
-  });
-
-  const onSubmit: SubmitHandler<TLoginFormValues> = async data => {
-    try {
-      const { email, password } = data;
-      localStorage.setItem('pendingEmail', email);
-
-      await signUpWithEmail(email, password, CONFIRM_EMAIL_LINK);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  ];
 
   return (
     <AuthLayoutWrapper>
-      <>
-        <h1 className="mb-5 text-h3 text-foreground">{t('signUp.title')}</h1>
-
-        <div className="flex flex-col w-full">
-          <Button variant="outline" onClick={() => null}>
-            {t('signUp.signUpWithGoogle')}
-          </Button>
-        </div>
-
-        <div className="flex w-full items-center">
-          <hr className="flex-1 border-border" />
-          <span className="px-4 text-muted-foreground text-sm">{t('common:span')}</span>
-          <hr className="flex-1 border-border" />
-        </div>
-
-        <form className="flex flex-col w-full gap-4" onSubmit={handleSubmit(onSubmit)}>
-          <FormInput
-            name="email"
-            control={control}
-            label={t('common:inputs.emailInput.label')}
-            placeholder={t('common:inputs.emailInput.placeholder')}
-            LeftIcon={Mail}
-            type="email"
-          />
-          <FormInput
-            name="password"
-            control={control}
-            label={t('common:inputs.passwordInput.label')}
-            placeholder={t('common:inputs.passwordInput.placeholder')}
-            LeftIcon={KeySquare}
-            type="password"
-          />
-
-          <div className="flex items-center justify-between text-muted-foreground text-sm">
-            <p>{t('signUp.hasAccount')}</p>
-            <AppLink title={t('signUp.login')} path={ROUTES.LOGIN} />
-          </div>
-
-          <Button variant="secondary" type="submit">
-            {t('signUp.signUpBtn')}
-          </Button>
-        </form>
-      </>
+      <AuthForm
+        title={t('signUp.title')}
+        googleBtnTxt={t('signUp.signUpWithGoogle')}
+        submitBtnTxt={t('signUp.signUpBtn')}
+        linksArr={links}
+      />
     </AuthLayoutWrapper>
   );
 };
