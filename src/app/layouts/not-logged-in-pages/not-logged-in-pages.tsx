545ae7e6d9c0e8d93c6cd1c17484d37eab0f6b13
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-17 22:26:15 +0300

    Add wrapper container to not logged in pages

--- src/app/layouts/not-logged-in-pages/not-logged-in-pages.tsx
+++ src/app/layouts/not-logged-in-pages/not-logged-in-pages.tsx
@@ -6,11 +6,11 @@ import { Spinner } from '@/shared/ui/spinner/spinner';
 
 const NotLoggedInPages = () => {
   return (
-    <>
+    <div className="px-4 py-8">
       <Suspense fallback={<Spinner className="absolute top-1/2 left-1/2 size-10 text-primary" />}>
         <Outlet />
       </Suspense>
-    </>
+    </div>
   );
 };
 
