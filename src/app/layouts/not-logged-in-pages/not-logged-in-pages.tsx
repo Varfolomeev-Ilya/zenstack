4d9b65698b7525b77ebc9a7686acf62238f0996a
@ivarfolomeev <i.varfolomeev@fundmates.com>
2026-02-18 18:16:16 +0300

    Improve not logged in pages container height

--- src/app/layouts/not-logged-in-pages/not-logged-in-pages.tsx
+++ src/app/layouts/not-logged-in-pages/not-logged-in-pages.tsx
@@ -6,7 +6,7 @@ import { Spinner } from '@/shared/ui/spinner/spinner';
 
 const NotLoggedInPages = () => {
   return (
-    <div className="px-4 py-8">
+    <div className="px-4 py-8 h-full">
       <Suspense fallback={<Spinner className="absolute top-1/2 left-1/2 size-10 text-primary" />}>
         <Outlet />
       </Suspense>
